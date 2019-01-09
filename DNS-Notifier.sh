0bfa5de94bd61ff67bee500c811d86cf79d32a10
Ashley Cawley <ash@ashleycawley.co.uk>
2019-01-09 22:51:15 +0000

    Working nicely

--- DNS-Notifier.sh
+++ DNS-Notifier.sh
@@ -6,14 +6,14 @@ SAVEIFS=$IFS
 # Changing the delimiter used by arrays from a space to a new line, this allows a list of users (on new lines) to be stored in to an array
 IFS=$'\n'
 
-find /var/named/ -maxdepth 1 -type f -name a* | sed s,/var/named/,,g | sed s/.db//g > domain-list.txt
+find /var/named/ -maxdepth 1 -type f | sort | sed s,/var/named/,,g | sed s/.db//g > domain-list.txt
 
 for DOMAIN in $(cat domain-list.txt)
 do
 	dig +short $DOMAIN A | grep "176.74.17.190" &>/dev/null
 	if [ `echo $?` == '0' ]
 	then
-		echo "$DOMAIN,176.74.17.190"
+		echo "$DOMAIN,`grep -ri "$DOMAIN" /var/cpanel/userdata/ | grep "main_domain: " | cut -d '/' -f 5`"
 	fi
 
 done
